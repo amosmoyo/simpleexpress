338e891852295a130c6bfe75dfcf58b44b72700e
amosmoyo <amosmoyo5300@gmail.com>
2020-07-07 14:09:37 +0300

    set a basic express server

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const express = require('express');
 
 const chalk = require('chalk');
+
+const http = require('http');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
+app.set('port', PORT);
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`The app is running on port ${PORT}`);
+})
